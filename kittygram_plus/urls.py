807001eecef21980bf046d817e29f228bb6a8e09
Sultan Bekbolat <ssb000ss@gmail.com>
2023-03-24 09:55:58 +0600

    Add owner model

--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,9 +1,7 @@
 from django.urls import include, path
-
 from rest_framework.routers import DefaultRouter
 
 from cats.views import CatViewSet
-
 
 router = DefaultRouter()
 router.register('cats', CatViewSet)
